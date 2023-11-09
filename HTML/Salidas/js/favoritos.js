e60a1621f043f4421542d1c86868b7e866df080a
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2023-11-08 18:00:27 -0600

    salidas

---
+++ HTML/Salidas/js/favoritos.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', () => {
+    // Llamada a la API para obtener los favoritos
+    fetch('json/favoritos.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                let div = document.createElement('div');
+                div.classList.add('favorito');
+                div.innerHTML = `
+                    <img src="${element.image}" alt="Imagen de ${element.nombre}">
+                    <h3>${element.name}</h3>
+                    <p>${element.description}</p>
+                    <p>${element.price}€</p>
+                    <button class="btn btn-danger">Eliminar</button>
+                    <button class="btn btn-warning">Editar</button>
+                    <button class="btn btn-success" onclick="window.location.href='#?add=${element.id}'">Añadir al carrito</button>
+                `;
+                document.getElementById('favoritos').appendChild(div);
+            })
+        })
+});
