d757392e669a3b376ffc2af3f3ad8efd3cd01347
Alex <alexurquidi1000.1@gmail.com>
2023-12-07 00:46:26 -0600

    arreglo fotitios y citas empleado

--- src/Empleado/js/citas.js
+++ src/Empleado/js/citas.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', () => {
+    /*
+    
     let id;
     fetch('./json/citas.json')
         .then(response => response.json())
@@ -44,4 +46,56 @@ window.addEventListener('load', () => {
         id = document.getElementById('citas').value;
         contenido();
     });
+
+    */
+
+    // Obtener las citas mediante fetch
+
+
 });
+
+function jennyqintana() {
+    alert("Jenny Quintana es mi jainita");
+    fetch('../json/citas.json')  // Reemplaza 'ruta/del/archivo.json' con la ruta correcta a tu archivo JSON
+        .then(response => response.json())
+        .then(citasJson => {
+            // Función para filtrar citas según la fecha seleccionada
+            function filtrarCitas() {
+                const fechaSeleccionada = document.getElementById('citas').value;
+
+                // Obtener la fecha actual
+                const fechaActual = new Date();
+
+                // Filtrar citas próximas para la fecha seleccionada
+                const citasFiltradas = citasJson.filter(cita => {
+                    const citaFecha = new Date(cita.fecha + 'T' + cita.hora);
+                    return citaFecha >= fechaActual && citaFecha.toISOString().split('T')[0] === fechaSeleccionada;
+                });
+
+                // Mostrar las citas filtradas en HTML
+                const citasContainer = document.getElementById('citas-container');
+                citasContainer.innerHTML = '';
+
+                if (citasFiltradas.length > 0) {
+                    citasFiltradas.forEach(cita => {
+                        const citaElement = document.createElement('div');
+                        citaElement.classList.add('cita');
+                        citaElement.innerHTML = `
+            <strong>Nombre:</strong> ${cita.nombre} <br>
+            <strong>Fecha:</strong> ${cita.fecha} <br>
+            <strong>Hora:</strong> ${cita.hora} <br>
+            <strong>Servicios:</strong> ${cita.servicios} <br>
+            <hr>
+          `;
+                        citasContainer.appendChild(citaElement);
+                    });
+                } else {
+                    citasContainer.innerHTML = '<p>No hay citas para la fecha seleccionada.</p>';
+                }
+            }
+
+            // Llamada inicial para mostrar todas las citas
+            filtrarCitas();
+        })
+        .catch(error => console.error('Error al obtener las citas:', error));
+}
