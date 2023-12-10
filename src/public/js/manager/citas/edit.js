8ccc67b51069e06ce7afaede3a0632f5cbed1594
Alex <alexurquidi1000.1@gmail.com>
2023-12-10 02:43:58 -0600

    edit de empleados y citas

--- src/public/js/manager/citas/edit.js
+++ src/public/js/manager/citas/edit.js
@@ -1,10 +1,37 @@
-window.addEventListener('load' , ev => {
-    let params = new URLSearchParams(location.search);
-    document.getElementById('id').value = params.get('id');
-    /*
-    document.getElementById('imagen').addEventListener('change' , ev => {
-        let imag = ev.target.files[0];
-         let blob = new Blob([imag], {type: imag.type});
-        document.getElementsByClassName('preview')[0].src = URL.createObjectURL(blob);
-    });*/
+import {
+    getToken,
+    getUserRoute,
+    getImageRoute,
+    getAppointmentRoute,
+    getUserSearchRoute,
+    getProductRoute
+} from "../../api.config.js";
+
+window.addEventListener('load', ev => {
+    let urlParams = new URLSearchParams(window.location.search);
+    let product = urlParams.get('id');
+    if (product == null) {
+        window.location.href = 'index.html';
+    }
+
+    let client_id = document.getElementById('cliente');
+    let birth_date = document.getElementById('fechaNac');
+    let sex = document.getElementById('sexo');
+    let phone = document.getElementById('telefono');
+    let email = document.getElementById('correo');
+
+    fetch(getAppointmentRoute(product))
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            // Asignar valores a los campos de entrada
+            name.value = data.name;
+            last_name.value = data.last_name;
+            birth_date.value = data.birth_date;
+            sex.value = data.sex;
+            phone.value = data.phone;
+            email.value = data.email;
+        })
+        .catch(err => console.log(err));
 });
--- src/public/js/manager/empleados/edit.js
+++ src/public/js/manager/empleados/edit.js
@@ -1,9 +1,39 @@
+import {
+    getToken,
+    getUserRoute,
+    getImageRoute,
+    getUserSearchRoute,
+    getProductRoute
+} from "../../api.config.js";
+
 window.addEventListener('load', ev => {
-    let params = new URLSearchParams(location.search);
-    document.getElementById('id').value = params.get('id');
-    document.getElementById('imagen').addEventListener('change', ev => {
-        let imag = ev.target.files[0];
-        let blob = new Blob([imag], { type: imag.type });
-        document.getElementsByClassName('preview')[0].src = URL.createObjectURL(blob);
-    });
+    let urlParams = new URLSearchParams(window.location.search);
+    let product = urlParams.get('id');
+    if (product == null) {
+        window.location.href = 'index.html';
+    }
+
+    let image = document.getElementById ('preview')
+    let name = document.getElementById('nombre');
+    let last_name = document.getElementById('apellido');
+    let birth_date = document.getElementById('fechaNac');
+    let sex = document.getElementById('sexo');
+    let phone = document.getElementById('telefono');
+    let email = document.getElementById('correo');
+
+    fetch(getUserRoute(product))
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            // Asignar valores a los campos de entrada
+            name.value = data.name;
+            image.src=getImageRoute(data.image_id)
+            last_name.value = data.last_name;
+            birth_date.value = data.birth_date;
+            sex.value = data.sex;
+            phone.value = data.phone;
+            email.value = data.email;
+        })
+        .catch(err => console.log(err));
 });
