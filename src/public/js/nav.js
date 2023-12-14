24286c8578a9bb748931421520bc6b45957e9fb2
Carlos Ramos <57056947+CarlosIRamosV@users.noreply.github.com>
2023-12-13 22:49:48 -0600

    test nav

---
+++ src/public/js/nav.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    /*=============== SHOW MENU ===============*/
+    let navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+    /* Menu show */
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+
+    /* Menu hidden */
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+});
